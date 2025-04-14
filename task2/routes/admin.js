b9b322c3006f275296dc3d53f18c871d2edb2c27
Aleksandr Feshchuk <aleksfeschuk@gmail.com>
2025-04-13 21:48:20 -0600

    create util/path.js, used method rootDir

--- task2/routes/admin.js
+++ task2/routes/admin.js
@@ -1,12 +1,15 @@
-const path = require('path');
+
 const express = require('express');
 const router = express.Router();
 
+const rootDir = require('../util/path');
+
+const path = require('path');
 
 const products = [];
 
 router.get('/add-product', (req, res) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    res.sendFile(path.join(rootDir, '..', 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res) => {
--- task2/routes/shop.js
+++ task2/routes/shop.js
@@ -1,12 +1,12 @@
-const path = require('path');
-
 const express = require('express');
 const router = express.Router();
+
+const rootDir = require('../util/path');
 const path = require('path');
 
 
 router.get('/', (req, res) => {
-    res.send(path.join(__dirname, '../', 'views', 'shop.html'));
+    res.sendFile(path.join(rootDir, '..', 'views', 'shop.html'));
 });
 
 
